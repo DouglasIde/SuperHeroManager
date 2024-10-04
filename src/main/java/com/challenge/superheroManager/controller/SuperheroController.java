f700aadbf3607bff56a014b9074646a08b3dfc36
DouglasIde <douglasymide@gmail.com>
2024-10-04 19:46:49 -0300

    Feat: Importing the Interface on the Controller

--- src/main/java/com/challenge/superheroManager/controller/SuperheroController.java
+++ src/main/java/com/challenge/superheroManager/controller/SuperheroController.java
@@ -1,5 +1,7 @@
 package com.challenge.superheroManager.controller;
 
+import com.challenge.superheroManager.repository.ISuperhero;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/superhero")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class SuperheroController {
+
+    @Autowired
+    private ISuperhero superheroRepository;
 }
