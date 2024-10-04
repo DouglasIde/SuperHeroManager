8c7df4a2dd40a374910b3894e564e6fbc565a161
DouglasIde <douglasymide@gmail.com>
2024-10-04 17:15:14 -0300

    first commit

---
+++ src/test/java/com/challenge/superheroManager/SuperheroManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.superheroManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
