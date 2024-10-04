c0b16feab5e7b566c5705abaa336d3a1fc4c5ae7
DouglasIde <douglasymide@gmail.com>
2024-10-04 19:48:59 -0300

    Feat: Creating the method "getAll"

--- src/main/java/com/challenge/superheroManager/controller/SuperheroController.java
+++ src/main/java/com/challenge/superheroManager/controller/SuperheroController.java
@@ -1,10 +1,15 @@
 package com.challenge.superheroManager.controller;
 
+import com.challenge.superheroManager.model.Superhero;
 import com.challenge.superheroManager.repository.ISuperhero;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/superhero")
@@ -13,4 +18,9 @@ public class SuperheroController {
 
     @Autowired
     private ISuperhero superheroRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Superhero>> getAll() {
+        return ResponseEntity.ok(superheroRepository.findAll());
+    }
 }
