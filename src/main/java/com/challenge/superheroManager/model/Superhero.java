65d0e4c2646a6b29f24c039e85e812bceb07314e
DouglasIde <douglasymide@gmail.com>
2024-10-04 18:49:30 -0300

    Feat: Creating the Superhero model with the heroes' data

--- src/main/java/com/challenge/superheroManager/model/Superhero.java
+++ src/main/java/com/challenge/superheroManager/model/Superhero.java
@@ -1,4 +1,45 @@
 package com.challenge.superheroManager.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tb_superheros")
 public class Superhero {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O atributo de Nome é obrigatório!")
+    private String name;
+
+    private String lastName;
+
+    @NotNull
+    private int age;
+
+    private String team;
+
+    @NotBlank(message = "O atributo de País é obrigatório!")
+    @Size(min = 3, max = 10, message = "O atributo de País deve ter 3 caracteres!")
+    private String country;
+
+    @NotBlank(message = "O atributo de Poder é obrigatório! Se não tiver, digite 'Sem poder'")
+    private String power;
+
+    private String power2;
 }
