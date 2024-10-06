ebdda2d17e4fdd4228df5fc3c071bee97dcf89e6
DouglasIde <douglasymide@gmail.com>
2024-10-06 16:52:34 -0300

    Feat: Creating the getByID method

--- src/main/java/com/challenge/superheroManager/controller/SuperheroController.java
+++ src/main/java/com/challenge/superheroManager/controller/SuperheroController.java
@@ -3,6 +3,7 @@ package com.challenge.superheroManager.controller;
 import com.challenge.superheroManager.model.Superhero;
 import com.challenge.superheroManager.repository.ISuperhero;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +20,13 @@ public class SuperheroController {
     @GetMapping
     public ResponseEntity<List<Superhero>> getAll() {
         return ResponseEntity.ok(superheroRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Superhero> getById(@PathVariable Long id) {
+        return superheroRepository.findById(id)
+                .map(response -> ResponseEntity.ok(response))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @GetMapping("/name/{name}")
