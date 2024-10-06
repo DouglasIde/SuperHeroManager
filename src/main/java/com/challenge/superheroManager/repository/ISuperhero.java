856e1079d756565e390c46fbf98fc10859cb4866
DouglasIde <douglasymide@gmail.com>
2024-10-06 16:30:59 -0300

    Feat: Implementing the Query Method in the repository

--- src/main/java/com/challenge/superheroManager/repository/ISuperhero.java
+++ src/main/java/com/challenge/superheroManager/repository/ISuperhero.java
@@ -3,7 +3,10 @@ package com.challenge.superheroManager.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.challenge.superheroManager.model.Superhero;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ISuperhero extends JpaRepository<Superhero, Long> {
-
+    public List<Superhero> findAllByNameContainingIgnoreCase(@Param("name")String name);
 }
