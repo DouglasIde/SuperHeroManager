eede6dace3a57f8375fc24d83afa0439ea34b430
DouglasIde <douglasymide@gmail.com>
2024-10-09 09:32:41 -0300

    Feat: Adding swagger-ui to the dependencies

--- src/main/java/com/challenge/superheroManager/controller/SuperheroControllerAdvice.java
+++ src/main/java/com/challenge/superheroManager/controller/SuperheroControllerAdvice.java
@@ -4,6 +4,8 @@ import com.challenge.superheroManager.exception.SuperheroNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
